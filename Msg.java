dd3e6151a954d5cbe5db61ee727ab6e7f5bab833
ruthik-dev <ruthik-dev@gmail.com>
2026-01-21 16:23:52 +0000

    ruthik added sub logic

--- Msg.java
+++ Msg.java
@@ -3,5 +3,7 @@ class Msg {
 		int a = 10, b = 20;
 		System.out.println("This is Naveen Java file");
 		System.out.println("The sum is: "+(a+b));
+		System.out.println("This is ruthik java project file");
+		System.out.println("The difference is: " + (a-b));
 	}
 }
